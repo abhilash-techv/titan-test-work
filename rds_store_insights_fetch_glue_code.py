f2a2310d98e68f88eae28d79151f02878762a19f
Abhilash Antony <abhilash.antony@techversantinfotech.com>
2026-01-30 10:59:52 +0530

    fetch store insights table

--- rds_store_insights_fetch_glue_code.py
+++ rds_store_insights_fetch_glue_code.py
@@ -13,13 +13,13 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # -------------------------------
-# READ FROM POSTGRES (TEST)
+# READ FROM POSTGRES
 # -------------------------------
 dyf = glueContext.create_dynamic_frame.from_options(
     connection_type="postgresql",
     connection_options={
         "url": "jdbc:postgresql://titan-database-1.chkeu0qa25jq.us-east-1.rds.amazonaws.com:5432/titanibdai2025",
-        "dbtable": "public.store_snapshot",
+        "dbtable": "public.store_insights",
         "secretId": "titan-rds-db-credentials-meera"
     }
 )
@@ -27,16 +27,16 @@ dyf = glueContext.create_dynamic_frame.from_options(
 print("Row count:", dyf.count())
 
 # -------------------------------
-# WRITE TO S3 AS CSV
+# WRITE TO S3 AS PARQUET
 # -------------------------------
 glueContext.write_dynamic_frame.from_options(
     frame=dyf,
     connection_type="s3",
     connection_options={
-        "path": "s3://titan-glue-test-data/postgres_check/",
+        "path": "s3://titan-glue-test-data/postgres_rds/store_insights/",
         "partitionKeys": []
     },
-    format="csv"
+    format="parquet"
 )
 
 job.commit()
